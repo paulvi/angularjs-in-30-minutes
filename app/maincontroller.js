be3804774ffffe7bd23cb9a570b1bb5d2d3347ee
Paul Verest <paul.verest@live.com>
2014-10-28 18:17:54 +0800

    commit everything to show in https://github.com/angelozerr/angularjs-eclipse/issues/120

---
+++ app/maincontroller.js
@@ -0,0 +1,64 @@
+app.controller("MainController", function MainController($scope){
+	$scope.understand = "I now understand how the scope works!";
+	$scope.inputValue = "";
+	
+	$scope.selectedPerson = 0;
+	$scope.selectedGenre = null;
+	$scope.people = [
+     {
+         id: 0,
+         name: 'Leon',
+         music: [
+             'Rock',
+             'Metal',
+             'Dubstep',
+             'Electro'
+         ],
+         live: true
+     },
+     {
+         id: 1,
+         name: 'Chris',
+         music: [
+             'Indie',
+             'Drumstep',
+             'Dubstep',
+             'Electro'
+         ],
+         live: true
+     },
+     {
+         id: 2,
+         name: 'Harry',
+         music: [
+             'Rock',
+             'Metal',
+             'Thrash Metal',
+             'Heavy Metal'
+         ],
+         live: false
+     },
+     {
+         id: 3,
+         name: 'Allyce',
+         music: [
+             'Pop',
+             'RnB',
+             'Hip Hop'
+         ],
+         live: true
+     }
+	];
+	$scope.newPerson = null;
+	$scope.addNew = function() {
+		alert("new");
+        if ($scope.newPerson != null && $scope.newPerson != "") {
+            $scope.people.push({
+                id: $scope.people.length,
+                name: $scope.newPerson,
+                live: true,
+                music: []
+            });
+        }
+    };
+});
